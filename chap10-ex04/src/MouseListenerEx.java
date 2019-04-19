375ba54e6d5f8b356b5127d9ece331d81a45deb6
USER <USER@AMP-12>
2019-04-19 19:50:42 +0900

    Commit

---
+++ chap10-ex04/src/MouseListenerEx.java
@@ -0,0 +1,44 @@
+import java.awt.Container;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import org.w3c.dom.events.MouseEvent;
+
+public class MouseListenerEx extends JFrame {
+	private JLabel la = new JLabel("Çï·Î¿ì");
+
+	public MouseListenerEx() {
+		setTitle("dlqpsxm dPWP");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.addMouseListener(new MyMouseListener());
+
+		c.setLayout(null);
+		la.setSize(50, 20);
+		la.setLocation(30, 30);
+		c.add(la);
+
+		setSize(250, 250);
+		setVisible(true);
+	}
+
+	class MyMouseListener implements MouseListener {
+		public void mousePressed(MouseEvent e) {
+			int x = e.getX();
+			int y = e.getY();
+			la.setLocation(x, y);
+		}
+		public void mouseReleased(MouseEvent e) {}
+		public void mouseClicked(MouseEvent e) {}
+		public void mouseEntered(MouseEvent e) {}
+		public void mouseExited(MouseEvent e) {}
+	}
+
+	public static void main(String[] args) {
+		new MouseListenerEx();
+
+	}
+
+}
