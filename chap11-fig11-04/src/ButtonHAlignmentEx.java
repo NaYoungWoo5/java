62aa0f785742a256ce03d59cfd87ca0446fd350d
NaYoungWoo5 <48539299+NaYoungWoo5@users.noreply.github.com>
2019-04-26 14:24:27 +0900

    Update ButtonHAlignmentEx.java

--- chap11-fig11-04/src/ButtonHAlignmentEx.java
+++ chap11-fig11-04/src/ButtonHAlignmentEx.java
@@ -8,12 +8,12 @@ import javax.swing.SwingConstants;
 
 public class ButtonHAlignmentEx extends JFrame {
 	public ButtonHAlignmentEx() {
-		setTitle("¹öÆ° Á¤·Ä");
+		setTitle("Â¹Ã¶Ã†Â° ÃÂ¤Â·Ã„");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new BorderLayout());
 
-		ImageIcon normalIcon = new ImageIcon("images/normalIcon.gif");// normalIcon¿ë ÀÌ¹ÌÁö
+		ImageIcon normalIcon = new ImageIcon("images/normalIcon.gif");
 		
 		JButton btn = new JButton("call~~", normalIcon);
 		btn.setHorizontalAlignment(SwingConstants.LEFT);
