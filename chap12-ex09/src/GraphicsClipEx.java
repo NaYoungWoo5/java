27263e35383562f6ca1f4fcd95475071e6d6117a
USER <USER@AMP-06>
2019-04-26 18:56:31 +0900

    Commit

---
+++ chap12-ex09/src/GraphicsClipEx.java
@@ -0,0 +1,42 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GraphicsClipEx extends JFrame {
+	private MyPanel panel = new MyPanel();
+
+	public GraphicsClipEx() {
+		setTitle("Å¬¸°Â¡");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+
+		setSize(300, 400);
+		setVisible(true);
+	}
+
+	class MyPanel extends JPanel {
+		private ImageIcon icon = new ImageIcon("images/image0.jpg");
+		private Image img = icon.getImage();
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setClip(100, 20, 150, 150);
+			g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+			g.setColor(Color.yellow);
+			g.setFont(new Font("ADAD", Font.ITALIC, 40));
+			g.drawString("go", 10, 150);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new GraphicsClipEx();
+	}
+
+}
