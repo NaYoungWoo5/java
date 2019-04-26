5477abcf5be5c7f7de9d0b1981c8d0b3e9ce0691
USER <USER@AMP-06>
2019-04-26 18:25:26 +0900

    Commit

---
+++ chap12-ex03/src/GraphicsColorFontEx.java
@@ -0,0 +1,42 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GraphicsColorFontEx extends JFrame {
+	private MyPanel panel = new MyPanel();
+
+	public GraphicsColorFontEx() {
+		setTitle("sdsad");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+
+		setSize(350, 470);
+		setVisible(true);
+	}
+
+	class MyPanel extends JPanel {
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setColor(Color.BLUE);
+			g.drawString("I Love JAVA", 30, 30);
+			g.setFont(new Font("Arial", Font.ITALIC, 30));
+			g.setFont(new Font("How much?", 30, 60));
+			g.setColor(new Color(0X00ff00ff));
+			for (int i = 1; i <= 5; i++) {
+				g.setFont(new Font("jd", Font.ITALIC, i * 10));
+				g.drawString("SDASD", 30, 60 + i * 60);
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new GraphicsColorFontEx();
+	}
+
+}
