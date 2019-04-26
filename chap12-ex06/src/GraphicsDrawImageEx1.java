ebd62e235f0ae79573a1b8b3bed4521f60800f16
USER <USER@AMP-06>
2019-04-26 18:37:44 +0900

    Commit

---
+++ chap12-ex06/src/GraphicsDrawImageEx1.java
@@ -0,0 +1,36 @@
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GraphicsDrawImageEx1 extends JFrame {
+	private MyPanel panel = new MyPanel();
+
+	public GraphicsDrawImageEx1() {
+		setTitle("dnsjqhs zmrlfh dnjsgksms dnlcldp dlalwl rmflrl");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+
+		setSize(300, 400);
+		setVisible(true);
+	}
+
+	class MyPanel extends JPanel {
+		private ImageIcon icon = new ImageIcon("images/image0.jpg");
+		private Image img = icon.getImage();
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+
+			g.drawImage(img, 20, 20, this);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GraphicsDrawImageEx1();
+	}
+
+}
